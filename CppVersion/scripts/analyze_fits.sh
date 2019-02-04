846c955670c2c1dee8f1662c5d2e31e7a7cb9bcd
David Swan <dswan@fredhutch.org>
2019-02-04 10:43:04 -0800

    additional fix for script directory

--- CppVersion/scripts/analyze_fits.sh
+++ CppVersion/scripts/analyze_fits.sh
@@ -20,13 +20,13 @@ else
 k=3
 fi
 
-nohup ./analyze_fits_0.sh $scale $model $k 2>&1 > /dev/null &
-nohup ./analyze_fits_1.sh $scale $model $k 2>&1 > /dev/null &
-nohup ./analyze_fits_2.sh $scale $model $k 2>&1 > /dev/null &
-nohup ./analyze_fits_3.sh $scale $model $k 2>&1 > /dev/null &
-nohup ./analyze_fits_4.sh $scale $model $k 2>&1 > /dev/null &
-nohup ./analyze_fits_5.sh $scale $model $k 2>&1 > /dev/null &
-nohup ./analyze_fits_6.sh $scale $model $k 2>&1 > /dev/null &
-nohup ./analyze_fits_7.sh $scale $model $k 2>&1 > /dev/null &
-nohup ./analyze_fits_8.sh $scale $model $k 2>&1 > /dev/null &
-nohup ./analyze_fits_9.sh $scale $model $k 2>&1 > /dev/null &
+nohup ../scripts/analyze_fits_0.sh $scale $model $k 2>&1 > /dev/null &
+nohup ../scripts/analyze_fits_1.sh $scale $model $k 2>&1 > /dev/null &
+nohup ../scripts/analyze_fits_2.sh $scale $model $k 2>&1 > /dev/null &
+nohup ../scripts/analyze_fits_3.sh $scale $model $k 2>&1 > /dev/null &
+nohup ../scripts/analyze_fits_4.sh $scale $model $k 2>&1 > /dev/null &
+nohup ../scripts/analyze_fits_5.sh $scale $model $k 2>&1 > /dev/null &
+nohup ../scripts/analyze_fits_6.sh $scale $model $k 2>&1 > /dev/null &
+nohup ../scripts/analyze_fits_7.sh $scale $model $k 2>&1 > /dev/null &
+nohup ../scripts/analyze_fits_8.sh $scale $model $k 2>&1 > /dev/null &
+nohup ../scripts/analyze_fits_9.sh $scale $model $k 2>&1 > /dev/null &
